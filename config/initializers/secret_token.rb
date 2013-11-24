72e72c9968f999b4d7080df7893481b4e6fadd79
Russell Smith <russell.smith@ukd1.co.uk>
2013-11-24 11:57:46 -0800

    Railz

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Converse::Application.config.secret_key_base = '212eac60bb720bf1344a4ca4be1b8a7f0d3e1ec7a05bae601d7c4050241f5c5030fc2e2346fbdc2f4386ceaaef00f80b485979a620c637f413eaf888c0c34506'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Converse::Application.config.session_store :cookie_store, key: '_converse_session'
