c607da59bcd40b371fb978d634dcd4a09107d400
Russell Smith <russell.smith@ukd1.co.uk>
2013-11-25 20:01:23 -0800

    Fix compass

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,12 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups(:assets => %w(development test))) 
 
 module Converse
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
